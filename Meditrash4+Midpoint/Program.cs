e415441182aa8ec0e3838519c905eb53050d3845
Filip-Pesula <filip.pesula@gmail.com>
2021-11-09 19:16:23 +0100

    Add project files.

---
+++ Meditrash4+Midpoint/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data;
+
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace Meditrash4_Midpoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connStr = "server=localhost;user=root;database=db0;port=3306;password=1955";
+            MySqlConnection conn = new MySqlConnection(connStr);
+
+            try
+            {
+                Console.WriteLine("Connecting to MySQL...");
+                conn.Open();
+
+                string sql = "SELECT COUNT(*) FROM Country";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                object result = cmd.ExecuteScalar();
+                if (result != null)
+                {
+                    int r = Convert.ToInt32(result);
+                    Console.WriteLine("Number of countries in the world database is: " + r);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            conn.Close();
+            Console.WriteLine("Done.");
+        }
+    }
+}
