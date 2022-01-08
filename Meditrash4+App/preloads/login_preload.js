c4334cd25806e25df6326cec1a7e3ad2cacd5841
Filip Glenda <filipglenda@seznam.cz>
2022-01-09 00:29:12 +0100

    added routing between app pages, thresh record addition and bases of other app parts

--- Meditrash4+App/preloads/login_preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron')
-
-contextBridge.exposeInMainWorld('tools', {
-    evaluateFormData: (data) => ipcRenderer.invoke('evaluate_login_data', data),
-    continueToMainPage: () => ipcRenderer.invoke('continue_to_main_page'),
-    getLoadedUserData: () => ipcRenderer.invoke('get_loaded_user_data')
-})
